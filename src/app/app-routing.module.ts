b4d388946c2a66bd79b943e28d0bdb2ff09f50da
careydevelopment <mrbrianmcarey@gmail.com>
2021-09-18 06:55:12 -0400

    navbar work

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./_layout/layout.module').then((m) => m.LayoutModule),
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
